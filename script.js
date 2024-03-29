809a177211e94aed51e647d81468e41f3f90d24b
Jeric John <jericjohnaligato@gmail.com>
2024-03-29 23:39:12 +0800

    style improvement

---
+++ script.js
@@ -0,0 +1,21 @@
+// const showMore = document.getElementById("showMore");
+// const figure = document.querySelector(".figure"); // Use querySelector for single element
+
+// showMore.addEventListener("click", () => {
+//   figure.style.left = "-100%"; // Set left position to -100%
+// });
+
+// showMore.addEventListener("click", () => {
+//     figure.classList.add("slide-left"); // Add a class with transition
+//   });
+  
+
+  function scroll_right(){
+    var right = document.querySelector("#slider");
+    const scrollAmount = slider.clientWidth * 1; // 100% of slider width
+    right.scrollBy(
+        scrollAmount,
+        0,
+        "smooth"
+      );
+  }
