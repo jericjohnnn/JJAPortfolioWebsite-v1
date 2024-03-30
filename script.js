e49870240c6f479740a0ef595e8ed421e4655be7
Jeric John <jericjohnaligato@gmail.com>
2024-03-30 18:16:03 +0800

    practicing js

--- script.js
+++ script.js
@@ -11,8 +11,66 @@
 // TODO IMITATE THE ANIMATION PROPERTY IN CSS WHERE ANIMATION IS INFINITE
 
 
+// function scroll_right() {
+//   var right = document.querySelector("#slider");
+//   const scrollAmount = slider.clientWidth * 1; // 100% of slider width
+//   // right.scrollBy(scrollAmount, 0, "smooth");
+//   right.animate(
+//     [
+
+//     ],
+//     {
+
+//     }
+//   );
+// }
+
+
 function scroll_right() {
-  var right = document.querySelector("#slider");
-  const scrollAmount = slider.clientWidth * 1; // 100% of slider width
-  right.scrollBy(scrollAmount, 0, "smooth");
+  const slider = document.querySelector(".figure");
+  const number = 100;
+  // const scrollAmount = slider.clientWidth; // Get slider width
+  
+  if (number == 0) {
+    slider.animate([
+      { left: '0%' },  
+      { left: '-100%' }  
+    ], {
+      duration: 1000,  // Animation duration in milliseconds
+      easing: 'ease-in',  // Adjust easing as needed
+      fill: "forwards",
+    });
+    number = 100;
+  } else if (number == 100) {
+    slider.animate([
+      { left: '0%' },  
+      { left: '-100%' }  
+    ], {
+      duration: 1000,  // Animation duration in milliseconds
+      easing: 'ease-in',  // Adjust easing as needed
+      fill: "forwards",
+    });
+    number = 200;
+  } else if (number == 200) {
+    slider.animate([
+      { left: '0%' },  
+      { left: '-100%' }  
+    ], {
+      duration: 1000,  // Animation duration in milliseconds
+      easing: 'ease-in',  // Adjust easing as needed
+      fill: "forwards",
+    });
+    number = 300;
+  } else {
+    slider.animate([
+      { left: '0%' },  
+      { left: '-100%' }  
+    ], {
+      duration: 1000,  // Animation duration in milliseconds
+      easing: 'ease-in',  // Adjust easing as needed
+      fill: "forwards",
+    });
+    number = 0;
+  }
+ 
 }
