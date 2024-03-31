974ad59346c5b80b1134739e2687bc7ee4e73b18
Jeric John <jericjohnaligato@gmail.com>
2024-03-31 18:19:25 +0800

    scripttt

--- script.js
+++ script.js
@@ -10,67 +10,40 @@
 //   });
 // TODO IMITATE THE ANIMATION PROPERTY IN CSS WHERE ANIMATION IS INFINITE
 
+const showMore = document.getElementById("showMore");
+const slider = document.querySelector(".figure");
+
+showMore.addEventListener("click", () => {
+  
+    // slider.animate([
+    //    { left: '-' + number1 + '%' },  
+    //    { left: '-'+ number2 + '%' }  
+    // ], {
+    //   duration: 1000,  // Animation duration in milliseconds
+    //   easing: 'ease-in',  // Adjust easing as needed
+    //   fill: "forwards",
+    // });
+
+  });
+
 
 // function scroll_right() {
-//   var right = document.querySelector("#slider");
-//   const scrollAmount = slider.clientWidth * 1; // 100% of slider width
-//   // right.scrollBy(scrollAmount, 0, "smooth");
-//   right.animate(
-//     [
-
-//     ],
-//     {
-
-//     }
-//   );
-// }
-
-
-function scroll_right() {
-  const slider = document.querySelector(".figure");
-  const number = 100;
-  // const scrollAmount = slider.clientWidth; // Get slider width
+//   const slider = document.querySelector(".figure");
+//   let number1 = 0;
+//   let number2 = 100;
+//   // const scrollAmount = slider.clientWidth; // Get slider width
   
-  if (number == 0) {
-    slider.animate([
-      { left: '0%' },  
-      { left: '-100%' }  
-    ], {
-      duration: 1000,  // Animation duration in milliseconds
-      easing: 'ease-in',  // Adjust easing as needed
-      fill: "forwards",
-    });
-    number = 100;
-  } else if (number == 100) {
-    slider.animate([
-      { left: '0%' },  
-      { left: '-100%' }  
-    ], {
-      duration: 1000,  // Animation duration in milliseconds
-      easing: 'ease-in',  // Adjust easing as needed
-      fill: "forwards",
-    });
-    number = 200;
-  } else if (number == 200) {
-    slider.animate([
-      { left: '0%' },  
-      { left: '-100%' }  
-    ], {
-      duration: 1000,  // Animation duration in milliseconds
-      easing: 'ease-in',  // Adjust easing as needed
-      fill: "forwards",
-    });
-    number = 300;
-  } else {
-    slider.animate([
-      { left: '0%' },  
-      { left: '-100%' }  
-    ], {
-      duration: 1000,  // Animation duration in milliseconds
-      easing: 'ease-in',  // Adjust easing as needed
-      fill: "forwards",
-    });
-    number = 0;
-  }
- 
-}
+//     slider.animate([
+//        { left: '-' + number1 + '%' },  
+//     { left: '-'+ number2 + '%' }  
+//     ], {
+//       duration: 1000,  // Animation duration in milliseconds
+//       easing: 'ease-in',  // Adjust easing as needed
+//       fill: "forwards",
+//     });
+   
+//     number1 = number1 + 100;
+//    number2 = number2 + 100;
+//     // { left: '-100%' },  
+//     //   { left: '-200%' } 
+// }
