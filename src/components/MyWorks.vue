090b552b6a3cf665161d3ec6d316ee6e3c6fbe39
JJ👨‍💻 <112809736+jericjohnnn@users.noreply.github.com>
2025-01-21 22:13:24 +0800

    update my works

--- src/components/MyWorks.vue
+++ src/components/MyWorks.vue
@@ -1,31 +1,48 @@
 <template>
-  <div class="bg-black min-h-screen container mx-auto px-4 py-14 text-white relative">
+  <div
+    :class="[
+      'min-h-screen container mx-auto px-4 py-14 text-white relative transition-colors duration-500',
+      activeTab === 'dev' ? 'bg-black' : 'bg-yellow',
+    ]"
+  >
     <div class="flex flex-col items-center mb-8">
-      <h2 class="text-4xl font-bold text-center text-yellow">MY WORKS</h2>
+      <h2
+        :class="[
+          'text-4xl font-bold text-center transition-colors duration-300',
+          activeTab === 'dev' ? 'text-yellow' : 'text-black',
+        ]"
+      >
+        MY WORKS
+      </h2>
     </div>
     <MyWorksWeb v-if="activeTab === 'dev'" />
     <MyWorksGfx v-if="activeTab === 'gfx'" />
 
-    <div class="flex items-center gap-2 p-1 bg-yellow rounded-full absolute bottom-8 right-8">
+    <div
+      :class="[
+        'flex items-center p-1 rounded-full absolute bottom-8 right-4 transition-colors duration-500 group',
+        activeTab === 'dev' ? 'bg-yellow' : 'bg-black',
+      ]"
+    >
       <button
         @click="activeTab = 'dev'"
         :class="[
-          'flex items-center gap-1 px-2 py-1 rounded-full transition-colors',
+          'flex items-center gap-1 p-1 rounded-full transition-all duration-500 ease-in-out w-16 justify-center',
           activeTab === 'dev' ? 'bg-black' : '',
         ]"
       >
-        <CodeXml size="16" :color="activeTab === 'dev' ? '#FFF' : '#0E161E'" />
-        <p class="text-sm" :class="activeTab === 'dev' ? 'text-white' : 'text-black'">Dev</p>
+        <CodeXml size="16" color="#F6E332" />
+        <p class="text-sm text-yellow">Dev</p>
       </button>
       <button
         @click="activeTab = 'gfx'"
         :class="[
-          'flex items-center gap-1 px-2 py-1 rounded-full transition-colors',
-          activeTab === 'gfx' ? 'bg-black' : '',
+          'flex items-center gap-1 p-1 rounded-full transition-all duration-500 ease-in-out w-16 justify-center',
+          activeTab === 'gfx' ? 'bg-yellow' : '',
         ]"
       >
-        <Palette :size="16" :color="activeTab === 'gfx' ? '#FFF' : '#0E161E'" />
-        <p class="text-sm" :class="activeTab === 'gfx' ? 'text-white' : 'text-black'">Gfx</p>
+        <Palette :size="16" color="#0E161E" />
+        <p class="text-sm text-black">Gfx</p>
       </button>
     </div>
   </div>
