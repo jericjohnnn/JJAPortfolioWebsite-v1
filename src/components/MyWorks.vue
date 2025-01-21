6b46b6c332d03933ba21b8eb3f4fc68e3a02265c
JJ👨‍💻 <112809736+jericjohnnn@users.noreply.github.com>
2025-01-22 00:09:05 +0800

    cleaned myworks section

--- src/components/MyWorks.vue
+++ src/components/MyWorks.vue
@@ -2,43 +2,43 @@
   <div
     :class="[
       'min-h-screen container mx-auto px-4 py-14 text-white relative transition-colors duration-500',
-      activeTab === 'dev' ? 'bg-black' : 'bg-yellow',
+      isOnDevView ? 'bg-black' : 'bg-yellow',
     ]"
   >
     <div class="flex flex-col items-center mb-8">
       <h2
         :class="[
           'text-4xl font-bold text-center transition-colors duration-300',
-          activeTab === 'dev' ? 'text-yellow' : 'text-black',
+          isOnDevView ? 'text-yellow' : 'text-black',
         ]"
       >
         MY WORKS
       </h2>
     </div>
-    <MyWorksWeb v-if="activeTab === 'dev'" />
-    <MyWorksGfx v-if="activeTab === 'gfx'" />
+    <MyWorksWeb v-if="isOnDevView" />
+    <MyWorksGfx v-if="!isOnDevView" />
 
     <div
       :class="[
         'flex items-center p-1 rounded-full absolute bottom-8 right-4 transition-colors duration-500 group',
-        activeTab === 'dev' ? 'bg-yellow' : 'bg-black',
+        isOnDevView ? 'bg-yellow' : 'bg-black',
       ]"
     >
       <button
-        @click="activeTab = 'dev'"
+        @click="toggleisOnDevView"
         :class="[
           'flex items-center gap-1 p-1 rounded-full transition-all duration-500 ease-in-out w-16 justify-center',
-          activeTab === 'dev' ? 'bg-black' : '',
+          isOnDevView ? 'bg-black' : '',
         ]"
       >
         <CodeXml size="16" color="#F6E332" />
         <p class="text-sm text-yellow">Dev</p>
       </button>
       <button
-        @click="activeTab = 'gfx'"
+        @click="toggleisOnDevView"
         :class="[
           'flex items-center gap-1 p-1 rounded-full transition-all duration-500 ease-in-out w-16 justify-center',
-          activeTab === 'gfx' ? 'bg-yellow' : '',
+          !isOnDevView ? 'bg-yellow' : '',
         ]"
       >
         <Palette :size="16" color="#0E161E" />
@@ -54,5 +54,9 @@ import MyWorksWeb from './MyWorksWeb.vue'
 import { CodeXml, Palette } from 'lucide-vue-next'
 import { ref } from 'vue'
 
-const activeTab = ref('dev')
+const isOnDevView = ref(true)
+
+const toggleisOnDevView = () => {
+  isOnDevView.value = !isOnDevView.value
+}
 </script>
