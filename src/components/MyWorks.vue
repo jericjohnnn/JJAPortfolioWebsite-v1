4d280317b74c968056881272a5d9cee4d227ecb0
JJ👨‍💻 <112809736+jericjohnnn@users.noreply.github.com>
2025-01-22 12:10:28 +0800

    adjust bg

--- src/components/MyWorks.vue
+++ src/components/MyWorks.vue
@@ -1,49 +1,51 @@
 <template>
   <div
     :class="[
-      'min-h-screen container mx-auto px-4 py-14 text-white relative transition-colors duration-500',
+      'min-h-screen text-white relative transition-colors duration-500',
       isOnDevView ? 'bg-black' : 'bg-yellow',
     ]"
   >
-    <div class="flex flex-col items-center mb-8">
-      <h2
-        :class="[
-          'text-4xl font-bold text-center transition-colors duration-300',
-          isOnDevView ? 'text-yellow' : 'text-black',
-        ]"
-      >
-        MY WORKS
-      </h2>
-    </div>
-    <MyWorksWeb v-if="isOnDevView" />
-    <MyWorksGfx v-if="!isOnDevView" />
+    <div class="container mx-auto px-4 py-14">
+      <div class="flex flex-col items-center mb-8">
+        <h2
+          :class="[
+            'text-4xl font-bold text-center transition-colors duration-300',
+            isOnDevView ? 'text-yellow' : 'text-black',
+          ]"
+        >
+          MY WORKS
+        </h2>
+      </div>
+      <MyWorksWeb v-if="isOnDevView" />
+      <MyWorksGfx v-if="!isOnDevView" />
 
-    <div
-      :class="[
-        'flex items-center p-1 rounded-full absolute bottom-8 right-4 transition-colors duration-500 group',
-        isOnDevView ? 'bg-yellow' : 'bg-black',
-      ]"
-    >
-      <button
-        @click="toggleisOnDevView"
+      <div
         :class="[
-          'flex items-center gap-1 p-1 rounded-full transition-all duration-500 ease-in-out w-16 justify-center',
-          isOnDevView ? 'bg-black' : '',
+          'flex items-center p-1 rounded-full absolute bottom-8 right-4 transition-colors duration-500 group',
+          isOnDevView ? 'bg-yellow' : 'bg-black',
         ]"
       >
-        <CodeXml size="16" color="#F6E332" />
-        <p class="text-sm text-yellow">Dev</p>
-      </button>
-      <button
-        @click="toggleisOnDevView"
-        :class="[
-          'flex items-center gap-1 p-1 rounded-full transition-all duration-500 ease-in-out w-16 justify-center',
-          !isOnDevView ? 'bg-yellow' : '',
-        ]"
-      >
-        <Palette :size="16" color="#0E161E" />
-        <p class="text-sm text-black">Gfx</p>
-      </button>
+        <button
+          @click="toggleisOnDevView"
+          :class="[
+            'flex items-center gap-1 p-1 rounded-full transition-all duration-500 ease-in-out w-16 justify-center',
+            isOnDevView ? 'bg-black' : '',
+          ]"
+        >
+          <CodeXml size="16" color="#F6E332" />
+          <p class="text-sm text-yellow">Dev</p>
+        </button>
+        <button
+          @click="toggleisOnDevView"
+          :class="[
+            'flex items-center gap-1 p-1 rounded-full transition-all duration-500 ease-in-out w-16 justify-center',
+            !isOnDevView ? 'bg-yellow' : '',
+          ]"
+        >
+          <Palette :size="16" color="#0E161E" />
+          <p class="text-sm text-black">Gfx</p>
+        </button>
+      </div>
     </div>
   </div>
 </template>
