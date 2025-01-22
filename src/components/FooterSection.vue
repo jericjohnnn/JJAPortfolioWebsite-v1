a493e139ebfb312b80f0095babb61acf1b694c29
JJ👨‍💻 <112809736+jericjohnnn@users.noreply.github.com>
2025-01-22 12:10:28 +0800

    adjust bg

--- src/components/FooterSection.vue
+++ src/components/FooterSection.vue
@@ -1,9 +1,11 @@
 <template>
-  <div class="bg-black container mx-auto px-4 py-5 text-neutral-400">
-    <div class="flex justify-between items-center">
-      <p class="text-xs font-light">© 2025 Made by Jeric John Aligato</p>
-      <div class="bg-yellow p-1 rounded-full">
-        <ChevronsUp :size="36" :stroke-width="1.5" class="text-black" />
+  <div class="bg-black text-neutral-400">
+    <div class="container mx-auto px-4 py-5">
+      <div class="flex justify-between items-center">
+        <p class="text-xs font-light">© 2025 Made by Jeric John Aligato</p>
+        <div class="bg-yellow p-1 rounded-full">
+          <ChevronsUp :size="36" :stroke-width="1.5" class="text-black" />
+        </div>
       </div>
     </div>
   </div>
