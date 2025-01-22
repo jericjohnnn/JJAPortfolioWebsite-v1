2639b8f534448313cf04378b492c628c8ebc02fa
JJ👨‍💻 <112809736+jericjohnnn@users.noreply.github.com>
2025-01-22 21:04:50 +0800

    scroll to sections

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,10 +1,34 @@
 <template>
   <div class="flex justify-between items-center">
     <img src="@/assets/jj_logo.webp" alt="JJ Logo" class="h-10" />
-    <Menu :size="40" color="#ffffff" class="cursor-pointer" />
+    <div>
+      <Menu :size="40" color="#ffffff" @click="isDrawerVisible = true" class="cursor-pointer" />
+      <Drawer
+        v-model:visible="isDrawerVisible"
+        position="full"
+        pt:header="!justify-end !p-4"
+        pt:content="!px-4"
+      >
+        <div class="flex flex-col justify-center items-center gap-20 text-4xl font-semibold py-16">
+          <a @click="emitSelectedSection('about')">ABOUT ME</a>
+          <a @click="emitSelectedSection('works')">MY WORKS</a>
+          <a @click="emitSelectedSection('contact')">CONTACT</a>
+        </div>
+      </Drawer>
+    </div>
   </div>
 </template>
 
 <script setup>
+import { Drawer } from 'primevue'
 import { Menu } from 'lucide-vue-next'
+import { ref } from 'vue'
+
+const isDrawerVisible = ref(false)
+const emit = defineEmits(['selectedSection'])
+
+const emitSelectedSection = (section) => {
+  isDrawerVisible.value = false
+  emit('selectedSection', section)
+}
 </script>
