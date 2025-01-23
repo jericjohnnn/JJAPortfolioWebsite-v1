2cdea43799c3572e6393156e01cfdd1b959adc44
JJ👨‍💻 <112809736+jericjohnnn@users.noreply.github.com>
2025-01-23 19:17:09 +0800

    disable dark mode

--- src/components/ContactMe.vue
+++ src/components/ContactMe.vue
@@ -6,7 +6,7 @@
       class="absolute inset-0 w-full h-full -z-10 object-cover"
     />
     <div class="container mx-auto p-4">
-      <div class="flex flex-col items-center justify-center gap-5 py-14">
+      <div class="flex flex-col items-center justify-center gap-5 py-14 text-black">
         <h2 class="text-4xl font-bold leading-relaxed">Get in touch</h2>
         <div class="text-lg text-center space-y-2">
           <p>jericjohnaligato@gmail.com</p>
