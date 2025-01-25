ee4a630882c6b3439a7289f18eb5b944f99ad49d
JJ👨‍💻 <112809736+jericjohnnn@users.noreply.github.com>
2025-01-25 20:27:13 +0800

    flicking plugin

--- src/components/MyWorksWeb.vue
+++ src/components/MyWorksWeb.vue
@@ -1,6 +1,60 @@
 <template>
   <div>
-    <div class="carousel">THIS IS A SAMPLE CAROUSEL</div>
+    <VueFlicking :options="{ circular: true }" :plugins="plugins">
+      <div class="card-panel is-small"><span class="flicking-index">1</span></div>
+      <div class="card-panel is-small"><span class="flicking-index">2</span></div>
+      <div class="card-panel is-small"><span class="flicking-index">3</span></div>
+      <div class="card-panel is-small"><span class="flicking-index">4</span></div>
+      <div class="card-panel is-small"><span class="flicking-index">5</span></div>
+      <div class="card-panel is-small"><span class="flicking-index">6</span></div>
+    </VueFlicking>
   </div>
 </template>
-<script setup></script>
+<script setup>
+import { Perspective } from "@egjs/flicking-plugins";
+
+const plugins = [new Perspective({ rotate: 0.5 })];
+</script>
+
+<style>
+.card-panel {
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  background-color: darkseagreen;
+  justify-content: flex-end;
+  border: 2px solid rgba(255, 255, 255, 0.1);
+  border-radius: 0.25rem;
+  box-shadow: 0 0.5em 1em -0.125em rgba(10.2, 10.2, 10.2, 0.1);
+  max-width: 100%;
+  font-size: 3rem;
+  margin: 0.5rem;
+  width: 50%;
+  height: 200px;
+  padding: 30px 20px;
+  box-sizing: border-box;
+  overflow: hidden;
+}
+
+.is-small {
+  width: 33%;
+}
+
+.flicking-index {
+  position: relative;
+  min-width: 60px;
+  text-align: center;
+}
+
+.flicking-index::after {
+  position: absolute;
+  content: "PANEL";
+  font-weight: bold;
+  left: 0;
+  bottom: -1.2rem;
+  width: 100%;
+  text-align: center;
+  font-size: 1rem;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import PrimeVue from 'primevue/config'
 import Aura from '@primevue/themes/aura'
 import Flicking from "@egjs/vue3-flicking";
 import "@egjs/vue3-flicking/dist/flicking.css";
+import "@egjs/flicking-plugins/dist/flicking-plugins.css";
 
 import App from './App.vue'
 import router from './router'
